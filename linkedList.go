1237896d25d67f53dc55263afd9a1a97c0c559f9
Armingodiz <armingodarzi1380@gmail.com>
2021-01-30 16:45:38 +0330

    linked list implemention tested

--- linkedList.go
+++ linkedList.go
@@ -127,15 +127,3 @@ func (list *LinkedList) Search(index int) *Node {
 		return node
 	}
 }
-
-func main() {
-	link := CreateLinkedList()
-	link.AddFront("head")
-	link.AddBack("new head")
-	link.AddBack("new new head")
-	link.AddFront("tail")
-	link.Display()
-	fmt.Println("######################################################3")
-	link.Delete(10)
-	link.Display()
-}
--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import "fmt"
+
+func main() {
+	link := CreateLinkedList()
+	link.AddFront("head")
+	link.AddBack("new head")
+	link.AddBack("new new head")
+	link.AddFront("tail")
+	link.Display()
+	fmt.Println("######################################################3")
+	link.Delete(10)
+	link.Display()
+}
