c3b6e6d2daa65a2722437e6ae5e5e5e0f28a511c
Armingodiz <armingodarzi1380@gmail.com>
2021-01-30 18:11:48 +0330

    structure of main added

--- main.go
+++ main.go
@@ -1,12 +1,47 @@
 package main
 
+import (
+	"fmt"
+)
+
+var cursorIndex int
+
 func main() {
-	link := CreateLinkedList()
-	link.AddFront("(")
-	link.AddFront("2")
-	link.AddFront("+")
-	link.AddFront("3")
-	link.AddFront(")")
-	link.AddAfterIndex(-1, "5")
-	link.Display()
+	cursorIndex = -1
+	var requests int
+	fmt.Scan(&requests)
+	var input string
+	fmt.Scan(&input)
+	list := CreateLinkedList()
+	for i := 0; i < len(input); i++ {
+		list.AddFront(string(input[i]))
+	}
+	list.Display()
+	var inp1 string
+	var inp2 string
+	for i := 0; i < requests; i++ {
+		fmt.Scan(&inp1)
+		switch inp1 {
+		case "<":
+			if cursorIndex != list.Size-1 {
+				cursorIndex += 1
+			}
+			break
+		case ">":
+			if cursorIndex != -1 {
+				cursorIndex -= 1
+			}
+			break
+		case "+":
+			fmt.Scan(&inp2)
+			break
+		case "-":
+			break
+		case "?":
+			break
+		case "!":
+			break
+
+		}
+	}
 }
